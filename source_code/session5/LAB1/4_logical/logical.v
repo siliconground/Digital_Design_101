2bd205b9cf3734989079cf156550956f99bcca2e
William Woo <codefoolosopher@gmail.com>
2026-04-08 22:49:57 +0900

    Revised some error

--- source_code/session5/LAB1/4_logical/logical.v
+++ source_code/session5/LAB1/4_logical/logical.v
@@ -5,13 +5,13 @@ reg [7:0] bar;
 
 initial begin
     foo = 45; bar = 9;
-    $display("Logical result for foo(%0d) && bar(%0d) : %0d", foo, bar, foo == bar);
+    $display("Logical result for foo(%0d) && bar(%0d) : %0d", foo, bar, foo && bar);
     foo = 0; bar = 4;
-    $display("Logical result for foo(%0d) && bar(%0d) : %0d", foo, bar, foo == bar);
+    $display("Logical result for foo(%0d) && bar(%0d) : %0d", foo, bar, foo && bar);
     foo = 'dx; bar = 3;
-    $display("Logical result for foo(%0d) && bar(%0d) : %0d", foo, bar, foo == bar);
+    $display("Logical result for foo(%0d) && bar(%0d) : %0d", foo, bar, foo && bar);
     foo = 'b101z; bar = 5;
-    $display("Logical result for foo(%0d) && bar(%0d) : %0d", foo, bar, foo == bar);
+    $display("Logical result for foo(%0d) && bar(%0d) : %0d", foo, bar, foo && bar);
     foo = 45; bar = 9;
     $display("Logical result for foo(%0d) || bar(%0d) : %0d", foo, bar, foo || bar);
     foo = 0; bar = 4;
